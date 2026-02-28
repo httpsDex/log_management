eeafcc84ef0ab85b86aab86b8981618dceab9c66
Dex ౨ৎ <franciscodexter308@gmail.com>
2026-02-28 22:24:37 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,5 @@
 // ─── API Configuration ──────────────────────────────────────────────────────
-// const API_BASE = 'http://localhost:3000/api';
-const API_BASE = 'https://log-management-3bou.onrender.com/api';
+const API_BASE = 'http://localhost:3000/api';
+
+
 
